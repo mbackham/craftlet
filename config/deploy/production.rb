9a38b40bb377946c6073ab9e794093aa7e83d934
mbackham <634264926@qq.com>
2026-06-17 14:55:03 +0800

    提交

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,11 +5,10 @@ server "38.76.215.163", user: "deploy", roles: %w[app db web]
 
 # SSH options
 set :ssh_options, {
-  keys: %w[~/.ssh/id_ed25519],
-  forward_agent: false,
-  auth_methods: %w[publickey]
+  keys: %w(~/.ssh/id_rsa),
+  forward_agent: true,
+  auth_methods: %w(publickey)
 }
-
 # Server-specific settings
 set :deploy_to, "/var/www/craftlet"
 set :rails_env, "production"
